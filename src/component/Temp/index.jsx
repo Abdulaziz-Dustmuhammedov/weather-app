e4fb986aeaae4dfc81ffb6479ad0433c51929128
AbdulAziz-210 <lexustoyota2002@gmail.com>
2024-12-24 14:21:20 +0500

    remove sun and add night img

--- src/component/Temp/index.jsx
+++ src/component/Temp/index.jsx
@@ -6,9 +6,9 @@ import humidity from "../../assets/icons/humidity.svg";
 import rain from "../../assets/icons/rain.svg";
 import search from "../../assets/icons/search.svg";
 
+import cloudyNight from "../../assets/images/partly-cloudy-night.png";
 import cloudImg from "../../assets/images/cloud.svg";
 import cloudyDay from "../../assets/images/partly-cloudy-day.png";
-import sunImg from "../../assets/images/sun.png";
 import cloudy from "../../assets/images/cloudy.png";
 import shover from "../../assets/images/showers.png";
 import heavyShowers from "../../assets/images/heavy-showers.png";
@@ -120,7 +120,7 @@ const Temp = () => {
           weather?.weather
             ? weather?.weather[0].icon === "01d" ||
               weather?.weather[0].icon === "01n"
-              ? sunImg
+              ? cloudyNight
               : weather?.weather[0].icon === "02d" ||
                 weather?.weather[0].icon === "02n"
               ? cloudyDay
@@ -201,95 +201,3 @@ const Temp = () => {
 };
 
 export default Temp;
-
-// const Temp = () => {
-//   const searchCity = () => {
-//     alert(inputVal);
-//   };
-
-//   const [inputVal, setInputVal] = useState("");
-
-//   const handleChange = (e) => {
-//     setInputVal(e.target.value);
-//     console.log(e.target.value);
-//   };
-
-//   return (
-//     <div className="temp">
-//       <img className="temp-cloud-img" src={cloudImg} alt="" />
-//       <div className="location-wrap cursor-pointer  ">
-//         <img className="" src={locationIcon} alt="" />
-//         <p className="location-text">Rio do Sul, SC</p>
-//       </div>
-//       <div className="input-wrap">
-//         <input
-//           type="search"
-//           className="search-input"
-//           placeholder="search"
-//           onChange={handleChange}
-//           value={inputVal}
-//         />
-//         <button onClick={searchCity} className="search-btn">
-//           <img
-//             className="search-img"
-//             src={search}
-//             alt="search img "
-//             width={30}
-//             height={30}
-//           />
-//         </button>
-//       </div>
-//       <div className="temp-text-folder">
-//         <p className="temp-text">
-//           18 <span>°C</span>
-//         </p>
-//         <p className="temp-sec-text">
-//           22° <span>16°</span>
-//         </p>
-//       </div>
-
-//       <div className="wrapper">
-//         <div className="wind-wrap flex items-center gap-3">
-//           <img
-//             className="opacity-50 ml-2"
-//             src={wind}
-//             alt=""
-//             width={27}
-//             height={24}
-//           />
-//           <div className="weather-wrap flex flex-col items-start ">
-//             <p className="wind-text">wind</p>
-//             <p className="wind-text">17 km/h</p>
-//           </div>
-//         </div>
-
-//         <div className="humidity-wrap flex items-center gap-3">
-//           <img className="ml-2" src={humidity} alt="" width={27} height={24} />
-//           <div className="weather-wrap flex flex-col items-start ">
-//             <p className="wind-text">humidity</p>
-//             <p className="wind-text">31 %</p>
-//           </div>
-//         </div>
-
-//         <div className="rain-wrap flex items-center gap-3">
-//           <img className="ml-2" src={rain} alt="" width={27} height={24} />
-//           <div className="weather-wrap flex flex-col items-start ">
-//             <p className="wind-text">rain</p>
-//             <p className="wind-text">10 %</p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Temp;
-
-// state = {
-//   inputVal: ""
-// };
-// handleChange = (e) => {
-//   this.setState(state{
-//     inputVal:e.target.value
-//   })
-// };
